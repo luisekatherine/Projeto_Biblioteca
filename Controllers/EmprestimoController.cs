887cfa17865f21942e7597ea38628829eeb6b9a5
Luise Katherine Arósquita <luisekatherine@hotmail.com>
2022-10-08 21:16:19 -0300

    Incluído a restrição de acesso às páginas

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -11,6 +11,8 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);
+
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -37,6 +39,8 @@ namespace Biblioteca.Controllers
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
+            Autenticacao.CheckLogin(this);
+
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
